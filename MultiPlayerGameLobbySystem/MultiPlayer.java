e9b3f3db5e5989ca97fc8e66dbd61da570f2cd29
nikitha-56 <nikithatalluri1@gmail.com>
2025-08-06 02:19:00 +0530

    Initial multiplayer game system with observer pattern and basic CLI

--- MultiPlayerGameLobbySystem/MultiPlayer.java
+++ MultiPlayerGameLobbySystem/MultiPlayer.java
@@ -3,7 +3,7 @@ class User{
     private String name;
     private String country;
     boolean gameCreator;
-    User(String name,String country,boolean gameCreator){
+    User(String name,String country){
         this.name=name;
         this.country=country;
         this.gameCreator=false;
@@ -17,8 +17,8 @@ class User{
         return country;
     }
 
-    public void createGame(User u){
-            u.gameCreator=true;
+    public void createGame(){
+         gameCreator=true;
     } 
 
     public boolean getCreator(){
@@ -26,19 +26,21 @@ class User{
     }
 }
 
+
 class GameManager{
-    public static GameManager instance;
-    int maxnoOfUsers;
-    List<User> users;
-    List<Observer> observers;
-    GameManager(){}
-    GameManager(int maxUsers){
-        this.maxnoOfUsers=maxUsers;
+    private static GameManager instance;
+    private int maxnoOfUsers;
+    private int joinedUsers;
+    private List<User> users;
+    private List<Observer> observers;
+    private GameManager(int maxnoOfUsers){
+        this.maxnoOfUsers=maxnoOfUsers;
         users=new ArrayList<>();
+        observers=new ArrayList<>();
     } 
-    public GameManager getInstance(GameManager manager){
-          if(manager==null){
-            instance=new GameManager();
+    public static GameManager getInstance(int maxnoOfUsers){
+          if(instance==null){
+            instance=new GameManager(maxnoOfUsers);
           }
           return instance;
     }
@@ -51,29 +53,24 @@ class GameManager{
         observers.remove(o);
     }
 
-    public boolean decrementUserCount(){
-         maxnoOfUsers--;
-         if(maxnoOfUsers<=0) return false;
-         return true;  
-    }
-
     public void addUser(User u){
         users.add(u);
-      boolean status= decrementUserCount();
-      for(Observer o:observers){
-            if(o.getClass().equals(NotifyToGameCreator.class)){
-                 o.notify(u);
-            }
-      }
-      if(status==false){
-         for(Observer o:observers){
-            if(o.getClass().equals(NotifyToUsers.class)){
-           o.notify(u);
-         }
+       joinedUsers++;
+       for(Observer o:observers){
+        if(o instanceof NotifyToUser || o instanceof NotifyToGameCreator){
+          o.notify(u);
         }
-      }
     }
-}
+    if(joinedUsers==maxnoOfUsers){
+        for(Observer o:observers){
+            if(o instanceof NotifyToUsers){
+                o.notify(u);
+            }
+        }
+    }
+    System.exit(0);
+       }
+    }
 
 interface Observer{
     void notify(User u);
@@ -81,25 +78,89 @@ interface Observer{
 
 class NotifyToGameCreator implements Observer{
     public void notify(User u){
-        System.out.println(u.getName()+ "joined the game" );
+        System.out.println("Notification to Game creator " + u.getName()+ " joined the game " );
     }
 }
 
 class NotifyToUser implements Observer{
     public void notify(User u){
-        System.out.println("You joined successfully");
+        System.out.println("Notification to user : You joined successfully");
     }
 }
 
 class NotifyToUsers implements Observer{
     public void notify(User u){
-        System.out.println("All Users Joined the Game,Starting the Game.....");
+        System.out.println("Notification to Users : All Users Joined the Game,Starting the Game.....");
+        System.exit(0);
     }
 }
 
 public class MultiPlayer {
+    public static Scanner sc=new Scanner(System.in);
+    GameManager manager=GameManager.getInstance(4);
+    public User collectUserDetails(){
+        System.out.print("Enter User name :");
+       String name=sc.next();
+       System.out.print("Enter country: ");
+       String country=sc.next();
+       User u=new User(name, country);
+       return u;
+        }
 
     public static void main(String[] args) {
-        
+        GameManager manager=GameManager.getInstance(4);
+         MultiPlayer game=new MultiPlayer();
+         List<User> users=new ArrayList<>();
+         while(true){
+        System.out.println("All operations in the game :");
+        System.out.println("1.Join the game,2.Create a Game,3.Add Observers,4 .Exit");
+        System.out.print("Enter the operation:" );
+        int n=sc.nextInt();
+        switch(n){
+            case 1:User u=game.collectUserDetails();
+                    users.add(u);
+                    manager.addUser(u);
+                    break;
+            case 2:
+            System.out.print("Enter the user details to make the game creator :");
+            String name=sc.next();
+            boolean found=false;
+              for(User us:users){
+                if(us.getName().equalsIgnoreCase(name)){
+                    us.createGame();
+                    System.out.println(name + "is the game creator");
+                    found=true;
+                    break;
+                }
+              }
+              if(!found){
+                System.out.println("User not found");
+              }
+              break;
+            case 3:
+            System.out.println("Enter the observer number to add:");
+            System.out.println("1.NotifyToGameCreator");
+            System.out.println("2.NotifyToUser");
+             System.out.println("3.NotifyToUsers");
+         int type=sc.nextInt();
+          Observer o=null;
+          switch(type){
+            case 1:o=new NotifyToGameCreator(); break;
+            case 2:o=new NotifyToUser(); break;
+            case 3:o=new NotifyToUsers() ; break;
+            default :System.out.println("Invalid observer type "); return;
+          }
+          manager.addObserver(o);
+              System.out.println("Observer added");
+             break;
+        case 4:System.out.println("Exiting the game...");
+             return;
+
+        default:
+        System.out.println("Invalid .pls try again");
+        return;
+        }
+    }
+         
     }
 }
